ecc897de8a504b038874a96bb283287a6a8155e8
kamal <dhimank079@gmail.com>
2022-04-16 23:12:00 +0530

    complete single ringtone

--- pages/ringtone/[id].js
+++ pages/ringtone/[id].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import React, { useState , useRef} from 'react'
+import React, { useState, useRef } from 'react'
 import PlayIcon from '../../assets/images/play-button-arrowhead.png'
 import PauseIcon from '../../assets/images/pause.png'
 import Image from 'next/image'
@@ -11,16 +11,13 @@ function Posts ({ data }) {
   const { id } = router.query
   const [showPlayButton, setPlayButton] = useState(true)
 
-  const audioRef = useRef();
+  const audioRef = useRef()
 
   const onLoadedMetadata = () => {
     if (audioRef.current) {
-        console.log(audioRef.current.duration);
+      console.log(audioRef.current.duration)
     }
-};
-
-  
-
+  }
 
   let playPause = () => {
     setPlayButton((showPlayButton = !showPlayButton))
@@ -40,26 +37,51 @@ function Posts ({ data }) {
         <span className='text-6xl uppercase font-extrabold'>{id}</span>
       </div>
       <div className='mx-10'>
-      <div className='bg-tonez-white h-2 my-10 rounded-full'>
-        <div className='bg-tonez-orange rounded-full h-6 w-6 relative top-[-7px]'></div>
+        <div className='bg-tonez-white h-2 my-10 rounded-full'>
+          <div className='bg-tonez-orange rounded-full h-6 w-6 relative top-[-7px]'></div>
+        </div>
+        <audio
+          id={detail.id}
+          src={detail.source_url}
+          ref={audioRef}
+          onLoadedMetadata={onLoadedMetadata}
+        ></audio>
+        <div className='flex justify-between'>
+          <div className=' w-10 '>
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              onClick={playPause}
+            >
+              <Image
+                className='cursor-pointer'
+                src={showPlayButton ? PlayIcon : PauseIcon}
+                alt='image'
+              />
+            </motion.button>
+          </div>
+          <div>
+            <span className='text-tonez-white text-4xl'> 00:00</span>
+          </div>
+        </div>
+        <div className='text-tonez-white text-center my-10'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className='my-10 text-tonez-white text-3xl flex space-x-10 flex justify-center'>
+          <div className='py-6 px-20 flex justify-center border-2 border-dashed border-tonez-white rounded-[100px]'>
+            <a href='/'>Download MP3</a>
+          </div>
+          <div className='py-6 px-20 flex justify-center border-2 border-dashed border-tonez-white rounded-[100px]'>
+            <a href='/'>Download MP4r</a>
+          </div>
+        </div>
       </div>
-      <audio id={detail.id} src={detail.source_url}  ref={audioRef} onLoadedMetadata={onLoadedMetadata}></audio>
-      <div className=' flex items-center justify-center w-10 my-10'>
-        <motion.button
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={playPause}
-        >
-          <Image
-            className='cursor-pointer'
-            src={showPlayButton ? PlayIcon : PauseIcon}
-            alt='image'
-          />
-        </motion.button>
-      </div>
-
-      </div>
-    
     </>
   )
 }
