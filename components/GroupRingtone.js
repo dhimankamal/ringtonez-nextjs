7544a23b38cb09df8db14d4b0acdd2895b33e8d4
kamal <dhimank079@gmail.com>
2022-05-12 21:33:50 +0530

    api integration with afc

--- components/GroupRingtone.js
+++ components/GroupRingtone.js
@@ -17,9 +17,9 @@ export default function GroupRingtone ({data,title,loading}) {
         {title}
       </span>
       <div className='text-left my-10 md:my-20 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8'>
-        {data.map((d,key) => {
+        {data?data.map((d,key) => {
               return  <SingleRingtonez key={key} title={d.title.rendered} id={d.id} Url={d.source_url} date={d.date} slug={d.slug}/> 
-        })}
+        }):'nodata'}
          
       </div>
       <div>
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import GroupRingtone from '../../components/GroupRingtone'
 import Image from 'next/image'
-import PostImage from '../../assets/images/posts/maxresdefault.jpg'
+//import PostImage from '../../assets/images/posts/maxresdefault.jpg'
 import axios from 'axios'
-//http://ringtonez.dhimaan.in/wp-json/wp/v2/media?parent=26
+
 export default function SinglePost () {
   const [loading, setLoading] = useState(true)
+  const [ringloading, setRingLoading] = useState(true)
   const [data, setData] = useState({})
+  const [ringdata, setRingData] = useState([])
   const router = useRouter()
   const { id } = router.query
 
@@ -14,68 +17,92 @@ export default function SinglePost () {
     setLoading((loading = true))
     try {
       const loaddata = await axios.get(
-        'http://ringtonez.dhimaan.in/wp-json/wp/v2/posts?slug=testing-title-2'
+        'https://ringtonez.dhimaan.in/wp-json/wp/v2/posts?slug=testing-title-2&_fields=acf,title'
       )
       setData((data = loaddata?.data[0]))
       setLoading((loading = false))
-      console.log('data : ', data)
+      loadringtone(data)
     } catch (e) {
       console.log('error', e)
       setLoading((loading = false))
     }
   }
 
+  const loadringtone = async (data) => {
+    await data.acf.ringtonearrrayurlmp3.split(',').map(async id => {
+      try {
+        const loaddata = await axios.get(
+          `https://ringtonez.dhimaan.in/wp-json/wp/v2/media?_fields=source_url,title,id,date&include=${id}`
+        )
+        setRingData(ringdata= [loaddata?.data[0], ...ringdata])
+      } catch (e) {
+        console.log('error', e)
+      }
+      console.log("ringdata1")
+    })
+    console.log("ringdata2")
+  }
+
   useEffect(() => {
     loadData()
   }, [])
   if (loading) return <>Loading</>
-  if (!loading){ return (
-    <>
-      <div className='flex justify-center w-full border-2 border-dashed border-tonez-white rounded-[50px] md:rounded-[100px] text-tonez-white py-16 md:py-36'>
-        <span className='text-2xl md:text-6xl uppercase font-extrabold'>
-        {data.title.rendered}
-        </span>
-      </div>
-
-      <div className='grid grid-cols-3 gap-10 m-10'>
-        {/* <div > {data.content.rendered} </div> */}
-        {/* <div className='col-span-2 space-y-10'>
-          <div>
-            <Image className='rounded-[20px]' src={PostImage} alt='image' />
-          </div>
-
-          <h2 className='text-tonez-white text-3xl font-bold'>
-            Trampoline Ringtones 2022 // Best Ringtones With Download Link
-          </h2>
-          <p className='text-tonez-white text-xl font-normal'>
-            Hey What's going on everybody, it's Dhiman back with most requested
-            video on our channel.Trampoline Ringtones 2022 // Best Ringtones
-            With Download Link
-          </p>
-          <h3 className='text-tonez-white text-3xl font-bold'>
-            Watch Video Tutorial
-          </h3>
-          <div>
-            <iframe
-              className='w-full h-96'
-              src='https://www.youtube.com/embed/3m_2DrFdd5g'
-              title='YouTube video player'
-              frameBorder='0'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-              allowFullScreen=''
-            ></iframe>
-          </div>
-          <h3 className='text-tonez-white text-3xl font-bold'>Download Now</h3>
-          <div></div>
-        </div> */}
-        <div className='grid gap-10'>
-          <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
-          <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
-          <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
-          <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
-          <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
+  if (!loading) {
+    return (
+      <>
+        <div className='flex justify-center w-full border-2 border-dashed border-tonez-white rounded-[50px] md:rounded-[100px] text-tonez-white py-16 md:py-36'>
+          <span className='text-2xl md:text-6xl uppercase font-extrabold'>
+            {data.title.rendered}
+          </span>
         </div>
-      </div>
-    </>
-  )
-}}
+
+        <div className='grid grid-cols-3 gap-10 m-10'>
+          <div className='col-span-2 space-y-10'>
+            <div>
+              <Image
+                className='rounded-[20px]'
+                src={data.acf.ImageUrl}
+                alt='image'
+                width='592'
+                height='400'
+              />
+            </div>
+
+            <h2 className='text-tonez-white text-3xl font-bold'>
+              {data.title.rendered}
+            </h2>
+            <p className='text-tonez-white text-xl font-normal'>
+              {data.acf.description}
+            </p>
+            <h3 className='text-tonez-white text-3xl font-bold'>
+              Watch Video Tutorial
+            </h3>
+            <div>
+              <iframe
+                className='w-full h-96'
+                src={data.acf.videourl}
+                title='YouTube video player'
+                frameBorder='0'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen=''
+              ></iframe>
+            </div>
+            <h3 className='text-tonez-white text-3xl font-bold'>
+              Download Now
+            </h3>
+            <div>
+              <GroupRingtone loading={ringloading} />
+            </div>
+          </div>
+          <div className='grid gap-10'>
+            <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
+            <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
+            <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
+            <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
+            <div className='h-96 border-2 border-dashed border-tonez-white rounded-[50px]'></div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
