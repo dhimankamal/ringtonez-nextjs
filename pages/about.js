71d128dc70221d11164dcf80678dbb6c16ebbb8a
kamal <dhimank079@gmail.com>
2022-05-28 12:27:10 +0530

    logo about us

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import PageHeader from '../components/PageHeader'
 import Logo from '../assets/images/logo.png'
 import Image from 'next/image'
 
-export default function about() {
+export default function about () {
   return (
     <>
       <Layout title='About - Ringtonez'>
@@ -22,15 +22,28 @@ export default function about() {
             </p>
             <h2 className='text-2xl font-bold'> Youtube Chanel</h2>
             <div className='mt-20 w-1/2 mx-auto'>
-              
-              <div className="rounded-3xl shadow-xl my-3 bg-tonez-white">
-          
-                <div className="flex justify-center mt-8">
-                <Image src={Logo} alt='logo' className="rounded-full border-solid border-white border-2 -mt-3" />
+              <div className='rounded-3xl shadow-xl my-3 bg-tonez-white'>
+                <div className='flex justify-center mt-8'>
+                  <div  className='rounded-full border-solid border-white border-2 -mt-3 bg-tonez-blue p-4'>
+                  <Image
+                    src={Logo}
+                    alt='logo'
+                   
+                  />
+                  </div>
+                 
                 </div>
-                <div className="text-center px-3 pb-6 pt-2">
-                  <h3 className="text-tonez-blue text-sm bold font-sans">Ringtonez</h3>
-                  <p className="mt-2 font-sans font-light text-tonez-blue">Ringtonez is a YouTube Channel where you will find all variety of ringtones.</p>
+                <div className='text-center px-3 pb-6 pt-2'>
+                  <h3 className='text-tonez-blue text-sm bold font-sans'>
+                    Ringtonez
+                  </h3>
+                  <p className='mt-2 font-sans font-light text-tonez-blue'>
+                    Ringtonez is a YouTube Channel where you will find all
+                    variety of ringtones.
+                  </p>
+                  <button class='mt-2 bg-slate-300 hover:bg-red-700 text-tonez-blue hover:text-white font-bold py-2 px-4 rounded'>
+                    Subscribe
+                  </button>
                 </div>
                 {/* <div className="flex justify-center pb-3 text-tonez-blue">
                   <div className="text-center mr-3 border-r pr-3">
@@ -44,7 +57,6 @@ export default function about() {
                 </div> */}
               </div>
             </div>
-
           </div>
         </div>
       </Layout>
