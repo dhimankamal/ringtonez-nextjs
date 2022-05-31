495537028e89971e8e5f037102befb6347c7f9b1
kamal <dhimank079@gmail.com>
2022-05-31 20:44:42 +0530

    fix error

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -64,31 +64,31 @@ export default function SinglePost () {
         <PostSkelton />
       </>
     )
-  // if (!data)
-  //   return (
-  //     <>
-  //       <div className='px-40 py-20 bg-white rounded-md shadow-xl'>
-  //         <div className='flex flex-col items-center'>
-  //           <h1 className='font-bold text-tonez-blue text-9xl'>404</h1>
+  if (!data)
+    return (
+      <>
+        <div className='px-40 py-20 bg-white rounded-md shadow-xl'>
+          <div className='flex flex-col items-center'>
+            <h1 className='font-bold text-tonez-blue text-9xl'>404</h1>
 
-  //           <h6 className='mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl'>
-  //             <span className='text-red-500'>Oops!</span> Post not found
-  //           </h6>
+            <h6 className='mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl'>
+              <span className='text-red-500'>Oops!</span> Post not found
+            </h6>
 
-  //           <p className='mb-8 text-center text-gray-500 md:text-lg'>
-  //             The page you’re looking for doesn’t exist.
-  //           </p>
+            <p className='mb-8 text-center text-gray-500 md:text-lg'>
+              The page you’re looking for doesn’t exist.
+            </p>
 
-  //           <a
-  //             href='#'
-  //             className='px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100'
-  //           >
-  //             Go home
-  //           </a>
-  //         </div>
-  //       </div>
-  //     </>
-  //   )
+            <a
+              href='#'
+              className='px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100'
+            >
+              Go home
+            </a>
+          </div>
+        </div>
+      </>
+    )
   if (!loading) {
     return (
       <>
