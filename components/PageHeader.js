504bbd06307248d59e6cf75f1255bfee764ffee1
kamal <dhimank079@gmail.com>
2022-09-05 21:03:35 +0530

    update title

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
 
 export default function PageHeader (props) {
   return (
-    <div className='text-center flex justify-center w-full border-2 border-dashed border-tonez-white rounded-[50px]   md:rounded-[100px] text-tonez-white p-10 md:py-28'>
-      <span className='text-3xl md:text-6xl uppercase font-extrabold'>{props.title}</span>
-      
-    </div>
+    <>
+      <Head>
+        <title>{props.title}</title>
+      </Head>
+      <div className='text-center flex justify-center w-full border-2 border-dashed border-tonez-white rounded-[50px] md:rounded-[100px] text-tonez-white p-10 md:py-28'>
+        <h1 className='text-3xl md:text-6xl uppercase font-extrabold'>
+          {props.title}
+        </h1>
+      </div>
+    </>
   )
 }
